adcf167c6d8f9080ab9655e84df7abc9566726c4
love-u-3000 <pavitra97agarwal@gmail.com>
2020-05-20 16:02:33 +0530

    For CSOC

--- site/code.js
+++ site/code.js
@@ -1,4 +1,19 @@
-function hello() {
-	document.getElementById('disp').innerHTML="Wow you should have not done that asshole";
-	document.getElementById('btn').innerHTML="Hide Me";
-}
+document.addEventListener("DOMContentLoaded",
+	function (event) {
+		function hello(event) {
+			console.log(event);
+			console.log("jskjsb");
+			if (event.shiftKey){
+				console.log(event.clientX);
+			}
+		}
+
+		function hey (event) {
+			console.log(event);
+		}
+
+		document.querySelector('button').addEventListener('click',hello);
+		document.querySelector('body').addEventListener('mousemove', hey);
+
+	}
+)
